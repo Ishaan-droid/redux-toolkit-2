90e1885a9ae95627d51fb39687acf01bbc3c375a
Ishaan Thakur <ishaant99@gmail.com>
2022-08-10 23:55:55 -0300

    RTK synchronous project

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,22 +1,35 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { selectAllUsers } from "../users/usersSlice";
 import { postAdded } from "./postsSlice";
 
 const AddPostForm = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [userId, setUserId] = useState("");
   const dispatch = useDispatch();
+
+  const users = useSelector(selectAllUsers);
 
   const onTitleChanged = (e) => setTitle(e.target.value);
   const onContentChanged = (e) => setContent(e.target.value);
+  const onAuhorChanged = (e) => setUserId(e.target.value);
 
   const onSavePostClicked = () => {
     if (title && content) {
-      dispatch(postAdded(title, content));
+      dispatch(postAdded(title, content, userId));
     }
     setTitle("");
     setContent("");
   };
+
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
+
+  const usersOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ));
 
   return (
     <section>
@@ -30,6 +43,11 @@ const AddPostForm = () => {
           value={title}
           onChange={onTitleChanged}
         />
+        <label htmlFor="postAuthor">Author:</label>
+        <select id="postAuthor" value={userId} onChange={onAuhorChanged}>
+          <option value=""></option>
+          {usersOptions}
+        </select>
         <label htmlFor="postContent">Content:</label>
         <textarea
           id="postContent"
@@ -37,7 +55,7 @@ const AddPostForm = () => {
           value={content}
           onChange={onContentChanged}
         />
-        <button type="button" onClick={onSavePostClicked}>
+        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
           Save Post
         </button>
       </form>
--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import PostAuthor from "./PostAuthor";
 import { selectAllPosts } from "./postsSlice";
 
 const PostLists = () => {
@@ -8,6 +9,9 @@ const PostLists = () => {
     <article key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content.slice(0, 100)}</p>
+      <p className="postCredit">
+        <PostAuthor userId={post.userId} />
+      </p>
     </article>
   ));
 
